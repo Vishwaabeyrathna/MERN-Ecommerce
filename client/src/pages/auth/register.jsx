0c94cfc3614b7dc2c4cce36545ac0a454efe3499
Vishwaabeyrathna <vishwamawanell@gmail.com>
2025-06-30 20:14:42 +0530

    tailwind css faild but project run no errors

---
+++ client/src/pages/auth/register.jsx
@@ -0,0 +1,13 @@
+function AuthRegister() {
+  return (
+    <div>
+      <div>Register</div>
+
+      <div className="bg-green-500 text-white p-6 text-xl">
+      ✅ Tailwind CSS is now working!
+    </div>
+    </div>
+  );
+}
+
+export default AuthRegister;
